f30f614c048a443ed6b3f8053156224cf14a66d2
Everyday Future <admin@theeverydayfuture.com>
2020-06-25 19:37:13 -0400

    remove f strings

--- rivetgame/launcher.py
+++ rivetgame/launcher.py
@@ -183,11 +183,11 @@ def main():
         # Draw a solid blue circle in the center
         pygame.draw.circle(screen, (0, 0, 255), (350, 350), 75)
 
-        img = font.render(f'Mode: {arduino.get_state()}', True, (0, 0, 0))
+        img = font.render('Mode: {}'.format(arduino.get_state()), True, (0, 0, 0))
         screen.blit(img, (20, 20))
-        img = font.render(f'Player 1 Points: {arduino.get_points(player_num=0)}', True, (0, 0, 0))
+        img = font.render('Player 1 Points: {}'.format(arduino.get_points(player_num=0)), True, (0, 0, 0))
         screen.blit(img, (20, 80))
-        img = font.render(f'Player 2 Points: {arduino.get_points(player_num=1)}', True, (0, 0, 0))
+        img = font.render('Player 2 Points: {}'.format(arduino.get_points(player_num=1)), True, (0, 0, 0))
         screen.blit(img, (20, 180))
 
         # Flip the display
