dbdd9c3c87434e3250cb4c3b64b0f668c423a8c2
datascraps <datascraps42@gmail.com>
2020-07-14 23:33:57 -0400

    Separated out screens file

--- rivetgame/launcher.py
+++ rivetgame/launcher.py
@@ -15,6 +15,8 @@ import struct
 import pygame
 from pygame.locals import *
 
+from screens import demo_screen, game_screen
+
 
 class ArduinoInterface:
     """Dedicated controller for a single Arduino"""
@@ -203,36 +205,6 @@ def main(arduino):
 
     # Done! Time to quit.
     pygame.quit()
-
-
-def demo_screen(arduino, screen, font):
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (350, 350), 75)
-
-    img = font.render('DEMO Mode: {}'.format(arduino.get_state()), True, (0, 0, 0))
-    screen.blit(img, (20, 20))
-    img = font.render('Player 1 Points: {}'.format(arduino.get_points(player_num=0)), True, (0, 0, 0))
-    screen.blit(img, (20, 80))
-    img = font.render('Player 2 Points: {}'.format(arduino.get_points(player_num=1)), True, (0, 0, 0))
-    screen.blit(img, (20, 180))
-
-
-def game_screen(arduino, screen, font):
-    # Fill the background with white
-    screen.fill((255, 255, 255))
-
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (350, 350), 75)
-
-    img = font.render('GAME Mode: {}'.format(arduino.get_state()), True, (0, 0, 0))
-    screen.blit(img, (20, 20))
-    img = font.render('Player 1 Points: {}'.format(arduino.get_points(player_num=0)), True, (0, 0, 0))
-    screen.blit(img, (20, 80))
-    img = font.render('Player 2 Points: {}'.format(arduino.get_points(player_num=1)), True, (0, 0, 0))
-    screen.blit(img, (20, 180))
 
 def run(controller_port):
     try:
