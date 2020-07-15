fc854bc19ec0e3358feb316b162643f58b4b78cf
datascraps <datascraps42@gmail.com>
2020-07-14 23:15:22 -0400

    Working arduino code

--- RivetGameController/RivetGameController.ino
+++ RivetGameController/RivetGameController.ino
@@ -1,54 +1,73 @@
 /*
- * Rivet Game
- * 
- * State-machine for Rivet game with 4 states - demo, training, game, and winner.
- * 
- * In game mode, write out 8-bit ids to LED's and scan them bit by bit with photodiode output.
- * 
- * Collect up higher-level data and write it out over serial.
- * 
- */
+   Rivet Game
+
+  State-machine for Rivet game with 4 states - demo, training, game, and winner.
+
+  In game mode, write out 8-bit ids to LED's and scan them bit by bit with photodiode output.
+
+  Collect up higher-level data and write it out over serial.
+
+*/
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <Arduino.h>
+#include <FastLED.h>
 
+
+#define NUM_LEDS 64
+#define NUM_LEDS_PER_ROW 8
+#define DATA_PIN 10
+#define CLOCK_PIN 11
+
+CRGB leds[NUM_LEDS];
+int led_states[NUM_LEDS];
 
 bool is_testing = false;
 bool do_print_diagnostics = false;
 
 // Inputs
-const int training_btn = 2;  // Start training
+const int training_btn = A10;  // Start training
 const int play_btn = 3;  // Start playing
 
 // Player A Inputs
-const int trigger_a = 4;  // trigger button
-const int motor_a = 5;    // Vibration motor to indicate rivet event
+//const int trigger_a = A11;  // trigger button
+const int trigger_a = 18;  // trigger button
+boolean trigger_a_ready = false;
+static unsigned long last_interrupt_time_a = 0;
+//const int motor_a = 5;    // Vibration motor to indicate rivet event
 const int photo_a = A0;   // Photodiode input pin
+const int photo_a_out = 12;   // Photodiode 5v pin
 uint8_t photothresh_a = 60; // Threshold value for photodiode reading
-uint8_t photoval_a = 0; // Parsed photodiode value
 Adafruit_BNO055 bno_a = Adafruit_BNO055(55, 0x28);
 float x_a, y_a, z_a = 0;
-int points_a = 0;
+double points_a = 0;
+int combo_a = 0;
+int max_combo_a = 0;
 
 // Player B Inputs
-const int trigger_b = 8;  // trigger button
-const int motor_b = 9;    // Vibration motor to indicate rivet event
-const int photo_b = A4;   // Photodiode input pin
+//const int trigger_b = A10;  // trigger button
+const int trigger_b = 19;  // trigger button
+boolean trigger_b_ready = false;
+static unsigned long last_interrupt_time_b = 0;
+//const int motor_b = 9;    // Vibration motor to indicate rivet event
+const int photo_b = A5;   // Photodiode input pin
+const int photo_b_out = 8;   // Photodiode 5v pin
 uint8_t photothresh_b = 60; // Threshold value for photodiode reading
-uint8_t photoval_b = 0; // Parsed photodiode value
 Adafruit_BNO055 bno_b = Adafruit_BNO055(55, 0x29);
 float x_b, y_b, z_b = 0;
-int points_b = 0;
+double points_b = 0;
+int combo_b = 0;
+int max_combo_b = 0;
 
 // Orientation windows
-int x_holster = 320;
 int y_holster = -20;
 int z_holster = 60;
-int x_rivet = 170;
-int y_rivet = -30;
-int z_rivet = 15;
+int y_rivet = 0;
+int z_rivet = 90;
+float gyro_thresh = 10.0;
 
 // States
 #define DEMO       0
@@ -59,67 +78,89 @@ int state = DEMO;
 
 // Counters
 unsigned long counter = 0;
-unsigned long state_timer = 0;
-unsigned long countdown = 0;
-unsigned long motor_a_countdown = 1000; // Limit how long the motor will vibrate the gun for a trigger pull (millis)
-unsigned long motor_b_countdown = 1000; // Limit how long the motor will vibrate the gun for a trigger pull (millis)
+//unsigned long state_timer = 0;
+//unsigned long countdown = 0;
+//unsigned long motor_a_countdown = 1000; // Limit how long the motor will vibrate the gun for a trigger pull (millis)
+//unsigned long motor_b_countdown = 1000; // Limit how long the motor will vibrate the gun for a trigger pull (millis)
 
 
 void setup() {
-  
-  // Set the idle timeout
-  state_timer = millis();
+
+  //  // Set the idle timeout
+  //  state_timer = millis();
 
   pinMode(training_btn, INPUT);
   pinMode(play_btn, INPUT);
-  pinMode(trigger_a, INPUT);
-  pinMode(trigger_b, INPUT);
+  // use input for analof current sense input
+  //  pinMode(trigger_a, INPUT);
+  //  pinMode(trigger_b, INPUT);
+  // Use input pullup to
+  pinMode(trigger_a, INPUT_PULLUP);
+  pinMode(trigger_b, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(trigger_a), on_trigger_a, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(trigger_b), on_trigger_b, CHANGE);
   pinMode(photo_a, INPUT);
   pinMode(photo_b, INPUT);
+  pinMode(photo_a_out, OUTPUT);
+  pinMode(photo_b_out, OUTPUT);
+  digitalWrite(photo_a_out, HIGH);
+  digitalWrite(photo_b_out, HIGH);
 
   // initialize serial communications at 115200 bps for status updates
   Serial.begin(115200);
   delay(1000);
-  
+
   /* Initialise the sensor */
-  if(!bno_a.begin())
+  if (!bno_a.begin())
   {
     serial_update("E", 0); // Error #0 - Gun A not connected
-//    while(1); // TODO - Uncomment to prevent the game from advancing after error
+    //    while(1); // TODO - Uncomment to prevent the game from advancing after error
   }
 
-// TODO - uncomment this once the addresses on the IMUs are set and both can be found.
-//  if(!bno_b.begin())
-//  {
-//    serial_update("E", 1); // Error #1 - Gun B not connected
-//    while(1);
-//  }
+
+  if (!bno_b.begin())
+  {
+    serial_update("E", 1); // Error #1 - Gun B not connected
+    //      while(1);
+  }
   /* Use external crystal for better accuracy */
   bno_a.setExtCrystalUse(true);
   bno_b.setExtCrystalUse(true);
+
+  LEDS.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+
+  Serial.println("SETUP");
 
 }
 
 
 /*
- * 
- * === Main Loop ===
- * 
- * Do diagnostic/cleanup steps that are common to all states and then drop into state-specific loops (next section)
- * 
+
+  === Main Loop ===
+
+  Do diagnostic/cleanup steps that are common to all states and then drop into state-specific loops (next section)
+
 */
 
 void loop() {
   counter = (counter + 1) % 1000000;
-  
+
+  if (trigger_a_ready) {
+    trigger(1);
+    trigger_a_ready = false;
+  }
+
+
+  if (trigger_b_ready) {
+    trigger(2);
+    trigger_b_ready = false;
+  }
+
   // Update the gun position data
   if (counter % 1000 == 0) {
     update_gun_positions();
   }
-  // Update the photodiode state - TODO: move into LED animation logic.
-  if (counter % 1000 == 0) {
-    update_photodiodes();
-  }
+
   // Animate the next frame of the current game mode.
   if (counter % 100 == 0) {
     switch (state) {
@@ -137,55 +178,99 @@ void loop() {
         break;
     }
   }
-  // Update the RasPi over serial on the current state.
-  if (counter % 100 == 0 && do_print_diagnostics == false) {
-    serial_update("S", state);
-  }
-  if (counter % 100 == 20 && do_print_diagnostics == false) {
-    serial_update("P", points_a);
-  }
-  if (counter % 100 == 40 && do_print_diagnostics == false) {
-    serial_update("p", points_b);
-  }
-  // OR print human-readable diagnostics over serial if specified.
-  if (counter % 1000 == 0 && do_print_diagnostics == true) {
-    print_diagnostics();
-  }
-  delayMicroseconds(10);
 
+  if (do_print_diagnostics) {
+    // print human-readable diagnostics over serial if specified.
+    if (counter % 1000 == 0) {
+      Serial.println(state);
+      print_diagnostics();
+    }
+  }
+  //  else {
+  //    // OR Update the RasPi over serial on the current state.
+  //    if (counter % 100 == 0) {
+  //      serial_update("S", state);
+  //    }
+  //    else if (counter % 100 == 10) {
+  //      serial_update("P", points_a);
+  //    }
+  //    else if (counter % 100 == 20) {
+  //      serial_update("p", points_b);
+  //    }
+  //    else if (counter % 100 == 30) {
+  //      serial_update("C", combo_a);
+  //    }
+  //    else if (counter % 100 == 40) {
+  //      serial_update("c", combo_b);
+  //    }
+  //    else if (counter % 100 == 50) {
+  //      serial_update("M", max_combo_a);
+  //    }
+  //    else if (counter % 100 == 60) {
+  //      serial_update("m", max_combo_b);
+  //    }
+  //  }
+
+  delayMicroseconds(10);
 }
 
 
 /*
- * 
- * === State Loops ===
- * 
- * Treat these like you normally would the main loop() function. But they'll only run in their active state.
- * 
+
+  === State Loops ===
+
+  Treat these like you normally would the main loop() function. But they'll only run in their active state.
+
 */
 
 void demo()
 {
-  // demo loop
-
-  // Check for trigger pull to exit demo mode
-  if (digitalRead(trigger_a) == LOW || digitalRead(trigger_b) == LOW) {
-    to_game();
+  double phase = sin(counter / 1000);
+  double freq = sin(counter / 10000);
+  // demo loop color wave
+  for (int LEDSerial = 0; LEDSerial < 64; LEDSerial++ ) {
+    int row = LEDSerial / NUM_LEDS_PER_ROW;
+    double col = (LEDSerial % NUM_LEDS_PER_ROW) / double(NUM_LEDS_PER_ROW);
+    double val_raw = ((row * freq) * 0.3) + phase;
+    double val = val_raw - floor(val_raw);
+    double diff = abs(val - col);
+    if (diff < 0.05) {
+      leds[LEDSerial] = CRGB(53, 38, 213);
+    }
+    else if (diff < 0.1) {
+      leds[LEDSerial] = CRGB(77, 112, 246);
+    }
+    else if (diff < 0.15) {
+      leds[LEDSerial] = CRGB(50, 217, 203);
+    }
+    else if (diff < 0.2) {
+      leds[LEDSerial] = CRGB(136, 236, 71);
+    }
+    else if (diff < 0.25) {
+      leds[LEDSerial] = CRGB(240, 244, 81);
+    }
+    else {
+      leds[LEDSerial] = CRGB(0, 0, 0);
+    }
   }
+
+  FastLED.show();
 }
 
 
 void training()
 {
   // training loop
+  for (int i = 0 ; i < 64 ; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
 }
 
 
 void game()
 {
-  // game loop (domo code just increments points)
-  points_a += 1;
-  points_b += 2;
+  // game loop
 }
 
 
@@ -195,18 +280,186 @@ void winner()
 }
 
 
+void on_trigger_a() {
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 300ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time_a < 300) {
+    return;
+  }
+  last_interrupt_time_a = interrupt_time;
+
+  switch (state) {
+    case DEMO:
+      to_training();
+      break;
+    case TRAINING:
+      to_game();
+      break;
+    case GAME:
+      trigger_a_ready = true;
+      break;
+    case WINNER:
+      to_demo();
+      break;
+  }
+}
+
+void on_trigger_b() {
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 300ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time_a < 300) {
+    return;
+  }
+  last_interrupt_time_a = interrupt_time;
+
+  switch (state) {
+    case DEMO:
+      to_training();
+      break;
+    case TRAINING:
+      to_game();
+      break;
+    case GAME:
+      trigger_b_ready = true;
+      break;
+    case WINNER:
+      to_demo();
+      break;
+  }
+}
+
+void trigger(int player) {
+  // Ignore holster shots
+  if (player == 1) {
+    if (is_holstered(x_a, y_a, z_a)) {
+      if (do_print_diagnostics) {
+        Serial.print("Holstered shot");
+      }
+      return;
+    }
+  }
+  else {
+    if (is_holstered(x_b, y_b, z_b)) {
+      if (do_print_diagnostics) {
+        Serial.print("Holstered shot");
+      }
+      return;
+    }
+  }
+
+  int led_index = -1;
+
+  // Check gyro
+  boolean gyro_correct;
+  if (player == 1) {
+    gyro_correct = is_rivet(x_a, y_a, z_a);
+  }
+  else {
+    gyro_correct = is_rivet(x_b, y_b, z_b);
+  }
+
+  if (gyro_correct) {
+    // Fire pattern to get led location
+    int photo_pin = photo_a;
+    if (player == 2) {
+      photo_pin = photo_b;
+    }
+    led_index = read_led(photo_pin) - 1;
+    if (do_print_diagnostics) {
+      Serial.println("GYRO correct");
+      Serial.println(led_index);
+    }
+  }
+  else if (do_print_diagnostics) {
+    Serial.println("GYRO incorrect");
+  }
+
+  // If gun is not postioned right led index will be 255 or -1
+  if (led_index >= 0 && led_index < NUM_LEDS) {
+    fire_sucess(led_index, player);
+  }
+  else {
+    fire_fail(player);
+  }
+
+  write_led_states();
+}
+
+void fire_sucess(int led_index, int player) {
+  // Ignore already riveted holes
+  if (led_states[led_index] != 0) {
+    return;
+  }
+
+  led_states[led_index] = player;
+
+  // Update scores
+  if (player == 1) {
+    points_a += 1;
+    combo_a += 1;
+    max_combo_a = max(combo_a, max_combo_a);
+  }
+  else {
+    points_b += 1;
+    combo_b += 1;
+    max_combo_b = max(combo_b, max_combo_b);
+  }
+
+  // Tell arduino about success for sound
+  serial_update("T", player);
+
+  if (have_won()) {
+    to_winner();
+  }
+
+  serial_update("P", points_a);
+  serial_update("p", points_b);
+  serial_update("C", combo_a);
+  serial_update("c", combo_b);
+  serial_update("M", max_combo_a);
+  serial_update("m", max_combo_b);
+}
+
+void fire_fail(int player) {
+  // Tell arduino about failure for sound
+  serial_update("t", player);
+
+  // Update scores
+  if (player == 1) {
+    combo_a = 0;
+  }
+  else {
+    combo_b = 0;
+  }
+
+  serial_update("P", points_a);
+  serial_update("p", points_b);
+  serial_update("C", combo_a);
+  serial_update("c", combo_b);
+  serial_update("M", max_combo_a);
+  serial_update("m", max_combo_b);
+}
+
+boolean have_won() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (led_states[i] == 0) return false;
+  }
+  return true;
+}
+
 /*
- * 
- * === State Transitions ===
- * 
- * Move to a new state and set/reset any necessary variables to get that state started.
- * 
+
+  === State Transitions ===
+
+  Move to a new state and set/reset any necessary variables to get that state started.
+
 */
 
 void to_demo()
 {
   // start demo loop
   state = DEMO;
+  serial_update("S", state);
 }
 
 
@@ -214,6 +467,7 @@ void to_training()
 {
   // start training loop
   state = TRAINING;
+  serial_update("S", state);
 }
 
 
@@ -223,6 +477,14 @@ void to_game()
   state = GAME;
   points_a = 0;
   points_b = 0;
+  combo_a = 0;
+  max_combo_a = 0;
+  combo_b = 0;
+  max_combo_b = 0;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    led_states[i] = 0;
+  }
+  serial_update("S", state);
 }
 
 
@@ -230,19 +492,20 @@ void to_winner()
 {
   // start winner loop
   state = WINNER;
+  serial_update("S", state);
 
-  // Winner loop animation timeout (milliseconds)
-  countdown = 10000;
-  state_timer = millis();
+  //  // Winner loop animation timeout (milliseconds)
+  //  countdown = 10000;
+  //  state_timer = millis();
 }
 
 
 /*
- * 
- * === IMU Functions ===
- * 
- * Parse the output from the gun positions and check that orientation is within different windows.
- * 
+
+  === IMU Functions ===
+
+  Parse the output from the gun positions and check that orientation is within different windows.
+
 */
 
 void update_gun_positions() {
@@ -252,7 +515,7 @@ void update_gun_positions() {
   x_a = event_a.orientation.x;
   y_a = event_a.orientation.y;
   z_a = event_a.orientation.z;
-  
+
   sensors_event_t event_b;
   bno_b.getEvent(&event_b);
   x_b = event_b.orientation.x;
@@ -260,54 +523,105 @@ void update_gun_positions() {
   z_b = event_b.orientation.z;
 }
 
-bool is_holstered(float x, float y, float z, float thresh=20) {
-  if (abs(x-x_holster) < thresh && abs(y-y_holster) < thresh && abs(z-z_holster) < thresh) {
+bool is_holstered(float x, float y, float z) {
+  int y_diff = min(abs(y - y_holster), abs(360 + y - y_holster));
+  int z_diff = min(abs(z - z_holster), abs(360 + z - z_holster));
+  if (y_diff < gyro_thresh && z_diff < gyro_thresh) {
     return true;
-  } else {
+  }
+  else {
     return false;
   }
 }
 
-bool is_rivet(float x, float y, float z, float thresh=20) {
-  if (abs(x-x_rivet) < thresh && abs(y-y_rivet) < thresh && abs(z-z_rivet) < thresh) {
+bool is_rivet(float x, float y, float z) {
+  int y_diff = min(abs(y - y_rivet), abs(360 + y - y_rivet));
+  int z_diff = min(abs(z - z_rivet), abs(360 + z - z_rivet));
+  if (y_diff < gyro_thresh && z_diff < gyro_thresh) {
     return true;
-  } else {
+  }
+  else {
     return false;
   }
 }
+
 
 
 /*
- * 
- * === Photodiode Functions ===
- * 
- * Parse the photodiode readings.
- * 
+    ==== LEDS ===
 */
 
-uint8_t get_photodiode_val(int diode_pin, int thresh=60) {
-  int temp_val = analogRead(diode_pin);
-  if (temp_val > thresh) {
-    return 0;
-  } else {
-    return 1;
+byte read_led(int diode_pin) {
+
+  byte result = 0;
+
+  for (int serialByte = 7; serialByte >= 0; serialByte-- ) {
+    for (int LEDSerial = 0; LEDSerial < 64; LEDSerial++ ) {
+      if (bitRead(LEDSerial + 1, serialByte)) {
+        leds[LEDSerial] = CRGB(255, 255, 255);
+      }
+      else {
+        leds[LEDSerial] = CRGB(0, 0, 0);
+      }
+
+    }
+
+    FastLED.show();
+
+    //    delay(50);
+    result = (result << 1) + byte(analogRead(diode_pin) < 600);
   }
+
+  // Error correction
+  byte result2 = 0;
+
+  for (int serialByte = 7; serialByte >= 0; serialByte-- ) {
+    for (int LEDSerial = 0; LEDSerial < 64; LEDSerial++ ) {
+      if (bitRead(LEDSerial + 1, serialByte)) {
+        leds[LEDSerial] = CRGB(255, 255, 255);
+      }
+      else {
+        leds[LEDSerial] = CRGB(0, 0, 0);
+      }
+
+    }
+
+    FastLED.show();
+
+    //    delay(50);
+    result2 = (result2 << 1) + byte(analogRead(diode_pin) < 600);
+  }
+
+  if (result != result2) {
+    return 0;
+  }
+
+  return result;
 }
 
-void update_photodiodes() {
-  photoval_a = get_photodiode_val(photo_a, photothresh_a);
-  photoval_b = get_photodiode_val(photo_b, photothresh_b);
+void write_led_states() {
+  for (int i = 0 ; i < NUM_LEDS ; i++) {
+    if (led_states[i] == 1) {
+      leds[i] = CRGB(127, 0, 0);
+    }
+    else if (led_states[i] == 2) {
+      leds[i] = CRGB(0, 127, 0);
+    }
+    else {
+      leds[i] = CRGB(0, 0, 50);
+    }
+  }
+  FastLED.show();
 }
-
 
 /*
- * 
- * === Serial Functions ===
- * 
- * Communucate over Serial to the Pi.
- * This implements no mechanism to get messages from the Pi because that shouldn't happen. 
- * Control is cleaner if it is one-way, so the Pi is just a follower of the Arduino.
- * 
+
+  === Serial Functions ===
+
+  Communucate over Serial to the Pi.
+  This implements no mechanism to get messages from the Pi because that shouldn't happen.
+  Control is cleaner if it is one-way, so the Pi is just a follower of the Arduino.
+
 */
 
 void serial_update(char* key, int val) {
@@ -330,7 +644,7 @@ void print_diagnostics() {
   Serial.print(y_b);
   Serial.print(" z_b=");
   Serial.print(z_b);
-//  Serial.print(" =");
-//  Serial.print();
+  //  Serial.print(" =");
+  //  Serial.print();
   Serial.println("");
 }
