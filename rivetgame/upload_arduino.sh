5f932381b528a1e9deda0e67b40db8c16ea50964
datascraps <datascraps42@gmail.com>
2020-07-18 14:20:07 -0400

    attempt to do headless arduino upload

--- rivetgame/upload_arduino.sh
+++ rivetgame/upload_arduino.sh
@@ -1,3 +1,6 @@
 cd /home/pi/rivetgame/RivetGameController
-ino build
-ino upload
+#sudo apt-get install xvfb -y
+Xvfb :4 -nolisten tcp -screen :$SCREEN 1280x800x24 &
+xvfb="$!"
+DISPLAY=:4 arduino --board arduino:avr:mega:cpu=atmega2560 --port /dev/ttyUSB0 --upload /home/pi/rivetgame/RivetGameController/RivetGameController.ino
+kill -9 $xvfb
