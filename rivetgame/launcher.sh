0b4b89740eb011270980fdf97379723456226f26
datascraps <datascraps42@gmail.com>
2020-07-18 01:41:08 -0400

    Try to install pi code on reboot

--- rivetgame/launcher.sh
+++ rivetgame/launcher.sh
@@ -1,5 +1,11 @@
 cd /home/pi/rivetgame/rivetgame
+echo "Waiting 5 seconds to connect to the internet"
 sleep 5
+echo "pulling github"
 git pull origin master
-sleep 1
+sleep 5
+echo "waiting 10 seconds for arduino to upload code"
+arduino --board arduino:avr:mega:cpu=atmega2560 --port /dev/ttyUSB0 --upload /home/pi/rivetgame/RivetGameController/RivetGameController.ino &
+arduino --board arduino:avr:mega:cpu=atmega2560 --port /dev/ttyUSB1 --upload /home/pi/rivetgame/RivetGameController/RivetGameController.ino &
+sleep 10
 python3 launcher.py
